27b8c88e5f1003854c41476df9cd7d461692384d
Kaival145 <kaival.s.shah@gmail.com>
2021-12-30 21:37:37 -0800

    Adding files + .csv

---
+++ model.py
@@ -0,0 +1,13 @@
+import PIL
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data_path = './data/cancer.csv'
+dataframe = pd.read_csv(data_path)
+dataframe_copy = pd.DataFrame.copy(dataframe)
+
+dataframe = dataframe[['diagnosis', 'perimeter_mean', 'radius_mean', 'texture_mean', 'area_mean', 'smoothness_mean', 'concavity_mean', 'symmetry_mean']]
+dataframe_copy['diagnosis'].replace({'M':1, 'B':0}, inplace = True)
+dataframe['diagnosis_binary'] = dataframe_copy['diagnosis'].astype('category').map({1: '1 (malignant)', 0: '0 (benign)'})
