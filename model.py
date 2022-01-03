a9a5dca5ae382c1b04a02b86f9af9773b17182ae
Kaival145 <kaival.s.shah@gmail.com>
2022-01-02 22:26:34 -0800

    Updating model + Adding dataplot.py file runner

--- model.py
+++ model.py
@@ -8,6 +8,7 @@ data_path = './data/cancer.csv'
 dataframe = pd.read_csv(data_path)
 dataframe_copy = pd.DataFrame.copy(dataframe)
 
-dataframe = dataframe[['diagnosis', 'perimeter_mean', 'radius_mean', 'texture_mean', 'area_mean', 'smoothness_mean', 'concavity_mean', 'symmetry_mean']]
+dataframe_copy = dataframe[['diagnosis', 'perimeter_mean', 'radius_mean', 'texture_mean', 'area_mean', 'smoothness_mean', 'concavity_mean', 'symmetry_mean']]
 dataframe_copy['diagnosis'].replace({'M':1, 'B':0}, inplace = True)
-dataframe['diagnosis_binary'] = dataframe_copy['diagnosis'].astype('category').map({1: '1 (malignant)', 0: '0 (benign)'})
+dataframe_copy['diagnosis_binary'] = dataframe_copy['diagnosis'].astype('category').map({1: '1 (malignant)', 0: '0 (benign)'})
+print(dataframe_copy.head())
